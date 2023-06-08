ae95cf3d8ff811079ab910ca8796740715e2b54b
潘么么 <panmeme@panmemedeMacBook-Pro.local>
2023-06-08 14:03:52 -0400

    verification level 2:encrypt password

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-
+const encrypt = require('mongoose-encryption');
 
 const app = express();
 
@@ -17,6 +17,8 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
+var secret = "thismysecret";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
 
 const User = mongoose.model("User", userSchema);
 
