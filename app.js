2f7d4d761d14b8ce3165e604e3e235d461f2c1fb
潘么么 <panmeme@panmemedeMacBook-Pro.local>
2023-06-08 13:18:48 -0400

    connect to mongodb

--- app.js
+++ app.js
@@ -10,8 +10,15 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-// const url = "mongodb+srv://qianwenpan:LpaU4z6r4Dxj4NjQ@cluster0.iyauopq.mongodb.net/secrectsDB";
-// mongoose.connect(url,{useNewUrlParser: true}).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err));
+const url = "mongodb+srv://qianwenpan:LpaU4z6r4Dxj4NjQ@cluster0.iyauopq.mongodb.net/secrectsDB";
+mongoose.connect(url,{useNewUrlParser: true}).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err));
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const User = mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -22,6 +29,8 @@ app.get("/login", (req, res) => {
 app.get("/register", (req, res) => {
     res.render("register");
 });
+
+
 
 app.listen(3000, ()=>{
     console.log("server runing");
